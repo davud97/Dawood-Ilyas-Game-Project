0eecc77b82b578e07922ce51513294421b210e2c
davud97 <dawoodilyas48@gmail.com>
2025-10-05 00:39:57 +0300

    created HTML, CSS, JS files

---
+++ script.js
@@ -0,0 +1,15 @@
+// global variables
+
+const bird = document.querySelector("bird")
+const target = document.querySelector("target")
+const blood = document.querySelector("blood")
+const scoreDisplay = document.querySelector("score")
+const bestDisplay = document.querySelector("best-score")
+const timerDisplay = document.querySelector("timer")
+const levelDisplay = document.querySelector("level")
+const gameArea = document.querySelector("game-area")
+
+let score = 0
+let bestScore = 0
+let timeLeft = 20
+let level = 1
